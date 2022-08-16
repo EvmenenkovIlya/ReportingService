916c31a7ba8dde836b435da7c66e63cd0d09ab43
Ilya <ilia.il.27@mail.ru>
2022-08-17 01:36:17 +0300

    Add stored procedures for LeadStatistic

---
+++ ReportingService.DB/Stored Procedures/LeadStatistic_Add.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[LeadStatistic_Add]
+	@DateStatistic date,
+	@LeadId bigint,
+	@TransactionCountForTwoMontht int,
+	@DepositsSum decimal,
+	@WithdrawSum decimal,
+	@TransferSum decimal,
+	@DepositCount bigint,
+	@WithdrawCount bigint,
+	@TransferCount bigint
+AS
+BEGIN
+	INSERT INTO dbo.LeadStatistic
+	(
+		[DateStatistic],
+		[LeadId],
+		[TransactionCountForTwoMonth],
+		[DepositsSum],
+		[WithdrawSum],
+		[TransferSum],
+		[DepositCount],
+		[WithdrawCount],
+		[TransferCount]
+	)
+    VALUES 
+	(
+		@DateStatistic,
+		@LeadId,
+		@TransactionCountForTwoMontht,
+		@DepositsSum,
+		@WithdrawSum,
+		@TransferSum,
+		@DepositCount,
+		@WithdrawCount,
+		@TransferCount
+	)
+SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]	
+
+END
---
+++ ReportingService.DB/Stored Procedures/LeadStatistic_GetById.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[LeadStatistic_GetById]
+	@Id bigint
+AS
+BEGIN
+	SELECT 
+		[Id],
+		[DateStatistic],
+		[LeadId],
+		[TransactionCountForTwoMonth],
+		[DepositsSum],
+		[WithdrawSum],
+		[TransferSum],
+		[DepositCount],
+		[WithdrawCount],
+		[TransferCount]
+		
+	FROM [dbo].[LeadStatistic]
+	WHERE Id = @Id
+END
