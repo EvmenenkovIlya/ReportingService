a0349995d9de72498b45521cd13627876e35f36c
Ilya <Evmenya@gmail.com>
2022-08-16 21:04:30 +0300

    Starts DataLayer

---
+++ ReportingService.DB/Stored Procedures/Transaction_Add.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [dbo].[Transaction_Add]
+	@AccountId bigint,
+	@Date datetime2,
+	@TransactionType tinyint,
+	@Ammount decimal(14,4),
+	@Currency smallint,
+	@Rate decimal(6,4)
+AS
+BEGIN
+	INSERT INTO dbo.[Transaction]
+	(
+		[AccountId],
+		[Date],
+		[TransactionType],
+		[Ammount],
+		[Currency],
+		[Rate]
+	)
+
+    VALUES 
+	(
+		@AccountId,
+		@Date,
+		@TransactionType,
+		@Ammount,
+		@Currency,
+		@Rate
+	)
+
+SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]	
+
+END
