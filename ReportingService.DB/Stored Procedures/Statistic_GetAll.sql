b8eb658a502888d4b9af240a0a353e8def9a7be7
Ilya <ilia.il.27@mail.ru>
2022-08-17 01:47:46 +0300

    Add storedprocedures for Statistic

---
+++ ReportingService.DB/Stored Procedures/Statistic_GetAll.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[Statistic_GetAll]
+AS
+BEGIN
+	SELECT 
+		[Id]	
+		[DateStatistic],
+		[DepositCount],
+		[WithdrawCount],
+		[TransferCount],
+		[DepositSum],
+		[WithdrawSum],
+		[TransferSum],
+		[ActiveAccountCount],
+		[AllAccountCount],
+		[ActiveLeadCount]
+		
+	FROM [dbo].[Statistic]
+END
+
