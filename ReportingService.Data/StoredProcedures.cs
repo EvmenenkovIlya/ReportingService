7428bccc93dcd2be37d1e9d3e27e362e4527433a
Ilya <Evmenya@gmail.com>
2022-08-19 00:32:31 +0300

    Add dapper

---
+++ ReportingService.Data/StoredProcedures.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportingService.Data;
+
+public class StoredProcedures
+{
+    public const string Transaction_GetAll = "Transaction_GetAll";
+    public const string Transaction_GetById = "Transaction_GetById";
+    public const string LeadInformation_Add = "LeadInformation_Add";
+    public const string LeadInformation_GetAll = "LeadInformation_GetAll";
+    public const string LeadInformation_GetById = "LeadInformation_GetById";
+    public const string LeadInformation_Update = "LeadInformation_Update";
+}
