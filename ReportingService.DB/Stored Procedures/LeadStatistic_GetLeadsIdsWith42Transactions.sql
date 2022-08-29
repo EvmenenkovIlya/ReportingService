1d73aa9345386d6d27f9cabbb55cdfebd924249b
Ilya <ilia.il.27@mail.ru>
2022-08-29 17:13:03 +0300

    Add endpoints for LeadUpdate

---
+++ ReportingService.DB/Stored Procedures/LeadStatistic_GetLeadsIdsWith42Transactions.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[LeadStatistic_GetLeadsIdsWith42Transactions]
+AS
+    BEGIN
+        SELECT [Id]
+    FROM dbo.LeadStatistic AS Statistic
+WHERE Statistic.TransactionCountForTwoMonth > 41
+END
