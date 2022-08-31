1ae31777390d7185671af0934bcd1340c0fcaf17
Ilya <Evmenya@gmail.com>
2022-08-31 14:58:24 +0300

    Add validation for birthdays

---
+++ ReportingService.API/Middleware/CustomExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text.Json;
+using ReportingService.Business.Exceptions;
+
+namespace ReportingService.API.Middleware;
+
+[ExcludeFromCodeCoverage]
+public class CustomExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<CustomExceptionHandlerMiddleware> _logger;
+
+    public CustomExceptionHandlerMiddleware(RequestDelegate next, ILogger<CustomExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (BadRequestException exception)
+        {
+            await HandleExceptionAsync(context, HttpStatusCode.BadRequest, exception.Message);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, HttpStatusCode statusCode, string message)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+
+        var result = JsonSerializer.Serialize(new { error = message });
+
+        return context.Response.WriteAsync(result);
+    }
+}
